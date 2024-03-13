9f8053288b0a546741a9625e0ec170df9aed96af
Fabien Sabatié <fabien.sabatie@live.fr>
2024-03-13 16:42:41 +0000

    show fabien

--- john/terminal/john.php
+++ john/terminal/john.php
@@ -1,15 +1,15 @@
 <?php
-    $a = 1; 
+    $a = "coco"; 
     $b = 2; 
     $c = 3;
-    $a = $b + $c;
-    $b = $a + $a;
-    $a = $b + $c;
-    $c = $a - $b;
+    $a = $b + $c; a = 2 + 3 
+    $b = $a + $a; b = 5 + 5
+    $a = $b + $c; a = 10 + 3
+    $c = $a - $b; c = 13 - 10
 
-    echo $a; // outputs : 2
-    echo $b; // outputs : 1
-    echo $c; // outputs : 1
+    echo $a; // outputs : 13
+    echo $b; // outputs : 10
+    echo $c; // outputs : 3
 
 ?>
 
