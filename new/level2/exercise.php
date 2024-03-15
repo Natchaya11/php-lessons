4cf71498d03c8b0f034c4464f92957ec92eefe72
soraya897 <65521790+soraya897@users.noreply.github.com>
2024-03-15 16:16:40 +0000

    show fabien

--- new/level2/exercise.php
+++ new/level2/exercise.php
@@ -1,14 +1,20 @@
 <?php 
-Class Vector 2D {
-    public $X
-    public $y
+class Vector2D {
+    public $X;
+    public $y;
 }
 
 // สร้างคลาส Vector2D พร้อมทั้ง x, y ใน constructor
 
 ?>
 
+}
 <?php
+class Vector3D extends Vector2D {
+    public function Z() {
+        echo $this->name ." Z\n";
+    }
+}
 
 // สร้างคลาส Vector3D ที่สืบทอดมาจากคลาส Vector2D และเพิ่มคุณสมบัติ z
 
