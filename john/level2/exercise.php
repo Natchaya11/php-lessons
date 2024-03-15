6b051fd8739b1b6d5f9a545887b2054ee7d880b9
Fabien Sabatié <fabien.sabatie@live.fr>
2024-03-15 14:05:00 +0000

    show fabien

---
+++ john/level2/exercise.php
@@ -0,0 +1,17 @@
+<?php
+
+// Make a Vector2D class with x, y in the constructor
+
+?>
+
+<?php
+
+// Create a Vector3D class that extends the Vector2D class and adds the z property
+
+?>
+
+<?php
+
+// Create the add method
+
+?>
